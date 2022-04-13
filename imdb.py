0627af1f111c5a1307527b881f90391ad69545a2
dineshbodala <ds176.edu@gmail.com>
2022-04-13 11:41:00 +0530

    ADDING CODE TO SCRAPE IMDB'S TOP 250 FILMS

--- imdb.py
+++ imdb.py
@@ -3,5 +3,11 @@ import requests
 
 source=requests.get('https://www.imdb.com/chart/top/')
 soup=BeautifulSoup(source.text,'html.parser')
+smovies=soup.find_all('td',class_='titleColumn')
+for i in smovies:
+    i=i.get_text()
+    print(i)
 
-print(soup)
+
+    
+
